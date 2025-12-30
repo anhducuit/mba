63ca64e3c6e60f878dfd47458c4eb1bbef1c5905
anhducuit <14520189@gm.uit.edu.vn>
2025-12-30 08:25:34 +0700

    Remove duplicate mba-main directory

--- mba-main/src/components/animation/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './animated-element';
-export * from './animated-list';
-export * from './motion-variants';
-export * from './scroll-reveal';
-export * from './hover-effects';
-export * from './animated-buttons';
--- mba-main/src/lib/formatters.ts
+++
@@ -1,8 +0,0 @@
-
-export function formatCurrency(amount: number): string {
-  return new Intl.NumberFormat('vi-VN', {
-    style: 'currency',
-    currency: 'VND',
-    maximumFractionDigits: 0
-  }).format(amount);
-}
